2d20ed18b1c5cb3b94898e2abe601b79cd4a4216
Cesar Salgado <cesarsvs@gmail.com>
2014-11-30 22:03:56 -0200

    Added VideoIter class.

--- video_utils.py
+++ video_utils.py
@@ -1,5 +1,7 @@
 import cv2
 import cv
+import os
+from cesarpy.io import get_all_file_names_from_dir
 import numpy as np
 
 def get_right_empty(h, w, nframes, want_gray, ravel, dtype):
@@ -47,3 +49,28 @@ def get_all_frames(path, want_gray=True, ravel=True, dtype=np.uint8):
   # When everything done, release the capture
   cap.release()
   return frames
+
+class VideoIter:
+  def __init__(self, path, ravel=False):
+    self.isfile = os.path.isfile(path)
+    if self.isfile:
+      self.cap = cv2.VideoCapture(path)
+      self.nframes = self.cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
+    else:
+      self.files = get_all_file_names_from_dir(path, sort=True, withpath=True)
+      self.nframes = len(self.files)
+    self.count = 0 
+  def get_next(self):
+    if self.isfile:
+      ok, frame = self.cap.read()
+    else:
+      if self.count >= self.nframes:
+        ok = False
+      else:
+        ok = True
+        frame = cv2.imread(self.files[self.count])
+    if not ok:
+      return None
+    else:
+      self.count += 1
+      return frame
